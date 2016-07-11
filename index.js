e96749081deee85469a618b91c0736e145386ac1
Nico <nicolas.schomberg@gmail.com>
2016-07-11 09:43:36 -0700

    Suggestions

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+/**
+ * This file is the entry point for the app.
+ * Here, we define the API endpoints and spin up the server.
+ */
+
 'use strict';
 
 //Set up the app to use the Express module
 let express = require('express');
 let app = express();
 
-//Set up json body parsing for POST requests
-let bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-app.use(bodyParser.json());
+//Load up the Ascii Service class
+let asciify = require('./lib/ascii-service');
 
-//Load up the Font Service class
-let fontService = require('./lib/font-service');
-
+//Set port
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/api/asciify', function(request, response) {
-	fontService.asciify(request, response);
-});
+//Suggest endpoint
+app.get('/asciify/suggest', asciify.suggest);
 
+//Resolve endpoint
+app.get('/asciify/resolve', asciify.resolve);
+
+//Spin up server
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
