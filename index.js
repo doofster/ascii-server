1aa06117f308170025feae4f018a44a11d61184d
Nico <nicolas.schomberg@gmail.com>
2016-07-12 20:45:50 -0700

    https

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ let app = express();
 let cors = require('cors');
 let bodyParser = require('body-parser');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // So we can POST.
 app.use(bodyParser.urlencoded({
   extended: true
